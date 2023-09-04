75a47a4bacfe05ad5375ff6241a5b8cdb97da947
Suigetsu <lagmed.019@gmail.com>
2023-09-04 16:51:17 +0100

    reversed everything

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 10:21:56 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/09/04 16:36:27 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/09/04 16:50:43 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,50 +43,22 @@ int	main(int ac, char **av)
 	mlx_image_to_window(mlx, img, 0, 0);
 	int i = 0, j = 0;
 	int x = 0, y = 0;
-	// int color = 0;
-	// while (var.map[i])
-	// 	printf("%s\n", var.map[i++]);
-	// i = 0;
-	int	**color;
-	color = malloc (sizeof(int *) * 11);
-	while (i < 11)
-	{
-		color[i] = malloc(sizeof (int) * 26);
-		i++;
-	}
-	i = 0;
-	int h = 0;
-	int f = 0;
+	int color;
 	while (var.map[i])
 	{
 		j = 0;
-		f = 0;
 		while (var.map[i][j])
 		{
 			if (var.map[i][j] == '1')
-				color[h][f] = 0xFFFFFFFF;
+				color = 0xFFFFFFFF;
 			else if (var.map[i][j] == '0')
-				color[h][f] = 0xFFFFFFFF;
+				color = 0xFFFFFFFF;
 			else if (var.map[i][j] == ' ')
-				color[h][f] = 0x00000000;
+				color = 0x00000000;
 			else
-				color[h][f] = 0xF5310446;
+				color = 0xF5310446;
 			j++;
-			f++;
 		}
-		h++;
-		i++;
-	}
-	h, f = 0;
-	while (x <= 32)
-	{
-		y = 0;
-		while (y <= 32)
-		{
-			mlx_put_pixel(img, x, y, color[h][f]);
-			y++;
-		}
-		x++;
 		i++;
 	}	
 	// mlx_put_pixel(img, 0, 0, 0xFF0000FF);
