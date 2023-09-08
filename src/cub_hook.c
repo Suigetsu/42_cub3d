820a4784b1ea9cc8939e669124d3df3d6928b301
Suigetsu <lagmed.019@gmail.com>
2023-09-08 16:47:21 +0100

    other movement buttons are working

--- src/cub_hook.c
+++ src/cub_hook.c
@@ -6,7 +6,7 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:51:47 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/09/08 16:13:31 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/09/08 16:46:00 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,45 @@ void	keyhook(mlx_key_data_t keydata, void *param)
 		exit(0);
 	else if (keydata.key == MLX_KEY_W && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		var->p.p_pos_x += 1;
+		// printf("%f\n", var->p.p_pos_y/T_SIZE);
+		if (var->map[(int)(var->p.p_pos_y / T_SIZE) - 1][(int)(var->p.p_pos_x / T_SIZE)] == '1')
+			return ;
+		var->p.p_pos_y -= 1;
 		draw_minimap(var, var->img);
 		draw_player_pixels(var, 0xFF378446, var->img);
         draw_line(var->img, var, 0x00000000); 
 	}
 	else if (keydata.key == MLX_KEY_S && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		printf("down\n");
+	{
+		var->p.p_pos_y += 1;
+		draw_minimap(var, var->img);
+		draw_player_pixels(var, 0xFF378446, var->img);
+        draw_line(var->img, var, 0x00000000); 
+	}
 	else if (keydata.key == MLX_KEY_D && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		printf("right\n");
+	{
+		var->p.p_pos_x += 1;
+		draw_minimap(var, var->img);
+		draw_player_pixels(var, 0xFF378446, var->img);
+        draw_line(var->img, var, 0x00000000); 
+	}
 	else if (keydata.key == MLX_KEY_A && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		printf("left\n");
+	{
+		var->p.p_pos_x -= 1;
+		draw_minimap(var, var->img);
+		draw_player_pixels(var, 0xFF378446, var->img);
+        draw_line(var->img, var, 0x00000000); 
+	}
 	else if (keydata.key == MLX_KEY_RIGHT && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
     {
-        rotate_player(var, 30); 
+        rotate_player(var, -3 * RADIANS); 
 		draw_minimap(var, var->img);
 		draw_player_pixels(var, 0xFF378446, var->img);
         draw_line(var->img, var, 0x00000000); 
     }
     else if (keydata.key == MLX_KEY_LEFT && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
     {
-        rotate_player(var, -30);
+        rotate_player(var, 3 * RADIANS);
 		draw_minimap(var, var->img);
 		draw_player_pixels(var, 0xFF378446, var->img);
         draw_line(var->img, var, 0x00000000);
