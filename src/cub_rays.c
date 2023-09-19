e165af79d28ea220833c266ee5ed88c09ecdb314
Suigetsu <lagmed.019@gmail.com>
2023-09-19 21:25:03 +0100

    another failed push

--- src/cub_rays.c
+++ src/cub_rays.c
@@ -6,7 +6,7 @@
 /*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:34:39 by mlagrini          #+#    #+#             */
-/*   Updated: 2023/09/19 21:19:01 by mlagrini         ###   ########.fr       */
+/*   Updated: 2023/09/19 21:24:21 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	cast_rays(t_cub *var)
 	i = 0;
 	// var->p.ray_angle = 0;
 	var->p.ray_angle = var->p.direction - 30;
-	while (i < var->x_max)
+	while (i < 1)
 	{
 		fix_any_angle(&var->p.ray_angle);
 		horizontal_distance(var);
@@ -56,6 +56,7 @@ float	horizontal_distance(t_cub *var)
 
 	dy = T_SIZE;
 	dx = T_SIZE / tan(var->p.ray_angle);
+	n_x = 1;
 	var->ray.endy_h = (int)(var->p.y / T_SIZE) * T_SIZE;
 	var->ray.endx_h = var->p.x + (var->p.y - var->ray.endy_h) / tan(var->p.ray_angle);
 	if (facing_up_down(var) == 0)
@@ -63,13 +64,15 @@ float	horizontal_distance(t_cub *var)
 		var->ray.endy_h += dy;
 		dy *= -1;
 	}
+	else if (facing_up_down(var) == -1)
+		n_x = 0;
 	if (facing_right_left(var) == 1)
 		dx *= -1;
 	n_x = var->ray.endx_h;
 	n_y = var->ray.endy_h;
 	if (facing_up_down(var) == 1)
 		var->ray.endy_h--;
-	while (var->ray.endx_h / T_SIZE >= 0 && var->ray.endx_h / T_SIZE < var->x_max && var->ray.endy_h / T_SIZE >= 0 && var->ray.endy_h / T_SIZE < var->y_max)
+	while (n_x && var->ray.endx_h / T_SIZE >= 0 && var->ray.endx_h / T_SIZE < var->x_max && var->ray.endy_h / T_SIZE >= 0 && var->ray.endy_h / T_SIZE < var->y_max)
 	{
 		printf("%f\n", var->ray.endx_h/T_SIZE);
 		if (check_wall(var, var->ray.endx_h, var->ray.endy_h))
