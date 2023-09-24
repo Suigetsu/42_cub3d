94ec1b419598a3ef00404309998d92c734732097
Suigetsu <lagmed.019@gmail.com>
2023-09-24 13:03:28 +0100

    more norminette issues are fixed

--- src/cub_texture.c
+++ src/cub_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub_texture.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrahmane <hrahmane@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mlagrini <mlagrini@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 08:57:09 by hrahmane          #+#    #+#             */
-/*   Updated: 2023/09/23 18:26:38 by hrahmane         ###   ########.fr       */
+/*   Updated: 2023/09/24 12:51:26 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 mlx_texture_t	*get_image(char *path)
 {
 	mlx_texture_t	*img;
-	
+
 	img = mlx_load_png(path);
-	if (!img)
-		
 	if (img->width != img->height)
 		printf("invalid texture.\n");
 	return (img);
@@ -29,7 +27,6 @@ void	get_tex(t_cub *var)
 	int	i;
 
 	i = 0;
-	// var->txt[0] = get_image(var->paths[0]);
 	while (i < 4)
 	{
 		var->txt[i] = get_image(var->paths[i]);
